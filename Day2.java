bc4a2b3ed753aba3fb9d87c7d58b2692fa7e1a27
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-03 16:06:47 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,21 @@
+public class Biodata {
+    public static void main(String[] args) {
+
+        // Menggunakan println() untuk menampilkan nama dan alamat
+        System.out.println("Nama            : Muh Adil Fachrezi");
+        System.out.println("Alamat          : Mamuju, Kalukku");
+
+        /*
+         * Menggunakan print() untuk menampilkan status
+         * tanpa langsung berpindah ke baris baru
+         */
+        System.out.print("Status          : ");
+        System.out.print("Mahasiswa\n");
+
+        /**
+         * Menggunakan printf() untuk menampilkan
+         * jenis kelamin dengan format %s
+         */
+        System.out.printf("Jenis Kelamin   : %s%n", "Laki-laki");
+    }
+}
