2f73f078bc1bd4d56fa2ca2aaec26f6a148a2da2
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-04 10:04:05 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // \n digunakan untuk membuat baris baru
+        System.out.println("Awali harimu\ndengan ngoding");
+
+        // \t digunakan untuk memberikan jarak seperti Tab
+        System.out.println("Rabu\t00.00-23.59\tDay1Ngoding");
+        System.out.println("Kamis\t00.00-23.59\tDay2Ngoding");
+
+        // \" digunakan untuk menampilkan tanda petik dua
+        System.out.println("Gwejh Berkata \"When yah Jago Ngoding!\"");
+
+        // \' digunakan untuk menampilkan tanda petik satu
+        System.out.println("Hari ini adalah \'Hari ke-3\'");
+
+        // \\ digunakan untuk menampilkan tanda backslash
+        System.out.println("C:\\Java\\Belajar");
+
+    }
+}
