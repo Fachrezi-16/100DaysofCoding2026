d6b21abe4f9d4f81281e86009dad2127776510b9
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-05 19:56:03 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,21 @@
+public class TipeDataNumerikBilanganBulat {
+    public static void main(String[] args) {
+
+        // byte digunakan untuk bilangan bulat kecil
+        byte jumlahKelas = 10;
+
+        // short digunakan untuk bilangan bulat
+        short jumlahMahasiswa = 300;
+
+        // int digunakan untuk bilangan bulat yang umum
+        int jumlahKomputer = 5000;
+
+        // long digunakan untuk bilangan bulat yang sangat besar
+        long jumlahData = 1000000000L;
+
+        System.out.println("Jumlah Kelas       : " + jumlahKelas);
+        System.out.println("Jumlah Mahasiswa   : " + jumlahMahasiswa);
+        System.out.println("Jumlah Komputer    : " + jumlahKomputer);
+        System.out.println("Jumlah Data        : " + jumlahData);
+    }
+}
