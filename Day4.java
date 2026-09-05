f7fa11852e911bb8c65ac63062c95afbb83b7fe5
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-05 20:19:30 +0800

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -1,16 +1,20 @@
-public class TipeDataNumerikBilanganBulat {
+public class DataNumerikBilanganBulat {
     public static void main(String[] args) {
 
-        // byte digunakan untuk bilangan bulat kecil
+        // byte digunakan untuk menyimpan bilangan bulat kecil
+        // Ukuran 8 bit, nilai -128 sampai 127
         byte jumlahKelas = 10;
 
-        // short digunakan untuk bilangan bulat
+        // short digunakan untuk menyimpan bilangan bulat yang lebih besar dari byte
+        // Ukuran 16 bit, nilai -32.768 sampai 32.767
         short jumlahMahasiswa = 300;
 
-        // int digunakan untuk bilangan bulat yang umum
+        // int digunakan untuk menyimpan bilangan bulat yang lebih besar
+        // Ukuran 32 bit, nilai -2.147.483.648 sampai 2.147.483.647
         int jumlahKomputer = 5000;
 
-        // long digunakan untuk bilangan bulat yang sangat besar
+        // long digunakan untuk menyimpan bilangan bulat yang sangat besar
+        // Ukuran 64 bit, nilai -9.223.372.036.854.775.808 sampai 9.223.372.036.854.775.807
         long jumlahData = 1000000000L;
 
         System.out.println("Jumlah Kelas       : " + jumlahKelas);
