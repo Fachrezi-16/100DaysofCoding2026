77e1dd27b6ff4978fea93ac5462f45aeb76ae218
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-06 20:31:46 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Menyimpan berat badan menggunakan tipe data float
+        // Tipe data float harus ditambahkan huruf f di belakang angka
+        float bb = 55.5f;
+
+        // Menyimpan tinggi badan menggunakan tipe data double
+        double tb = 160.25;
+
+        // Menampilkan nilai menggunakan printf
+        System.out.printf("%f %f", bb, tb);
+
+    }
+}
