8d2c0e4700f67e48ad6911e00b7fc9b5319269a9
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-07 13:37:02 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,25 @@
+public class TipeDataKarakter {
+    public static void main(String[] args) {
+        /*
+        Jefri adalah mahasiswa baru. Dia mau bikin program
+        sederhana umtuk mendata dirinya sendiri
+        Dari data berikut:
+        */
+        // 1. Umur Jefri = 20 tahun
+        // 2. IPK Jefri = 3.75
+        // 3. Kode Jurusan Jefri = "I" untuk Informatika
+        // 4. Status Jefri = masih aktif kuliah
+             
+        String nama = "Jefri";
+        int umur = 20;
+        double ipk = 3.75;
+        char kodeJurusan = 'i';
+        boolean Status = true;
+
+        System.out.println("Nama :" + nama);
+        System.out.println("Umur : " + umur);
+        System.out.println("IPK : " + ipk);
+        System.out.println("Kode Jurusan : " + kodeJurusan);
+        System.out.println("Status : " +  Status);
+    }
+}
