e532fa53ebee0f0d8f5ac6cd7a27f83990d821f1
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-10 20:14:44 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // final membuat nilai tidak dapat diubah
+        final String nama = "Adil";
+        final int umur = 18;
+        final String jurusan = "Informatika";
+
+        System.out.println("Nama    : " + nama);
+        System.out.println("Umur    : " + umur);
+        System.out.println("Jurusan : " + jurusan);
+    }
+}
