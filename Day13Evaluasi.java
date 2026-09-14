a537ef00d08fa889974344f2c738b871464974d6
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-14 22:10:11 +0800

    Create Day13Evaluasi.java

---
+++ Day13Evaluasi.java
@@ -0,0 +1,9 @@
+public class Evaluasi {
+    public static void main(String[] args) {
+       double nilai1 = 2.56;
+       double nilai2 = 3.14;
+
+       System.out.printf("%.2f\n%.2f " , nilai1 , nilai2 );
+
+    }
+}
