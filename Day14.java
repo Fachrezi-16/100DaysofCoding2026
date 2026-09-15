3110f8d170ca76a0b97ff992b935b91d2199c5cb
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-15 22:29:32 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,12 @@
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int hasilPenjumlahan = a + b;
+        int hasilPengurangan = a - b;
+
+        System.out.println("Hasil penjumlahan: " + hasilPenjumlahan);
+        System.out.println("Hasil pengurangan: " + hasilPengurangan);
+    }
+}
