78fa0975dce18421cb734ee65ace31eec51d149a
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-24 20:38:57 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+
+        int sisi = 10;
+
+        int luas = sisi * sisi;
+
+        System.out.println("Sisi persegi: " + sisi);
+        System.out.println("Luas persegi: " + luas);
+    }
+}
