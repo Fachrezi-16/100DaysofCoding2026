113d4c2e82597c43477188beba3537860209939b
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-25 22:16:52 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,13 @@
+public class LuasPersegiPanjang {
+    public static void main(String[] args) {
+
+        int panjang = 12;
+        int lebar = 6;
+
+        int luasPersegiPanjang = panjang * lebar;
+
+        System.out.println("Panjang = " + panjang);
+        System.out.println("Lebar = " + lebar);
+        System.out.println("Luas Persegi Panjang = " + luasPersegiPanjang);
+    }
+}
