e67e167d5af411f92878b8da6951bd52ec96ba50
Fachrezi-16 <fchrzy6@gmail.com>
2026-09-26 22:31:28 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,10 @@
+public class LuasLingkaran {
+    public static void main(String[] args) {
+
+        double radius = 10;
+        double luas = Math.PI * radius * radius;
+
+        System.out.println("Radius = " + radius);
+        System.out.printf("Luas Lingkaran = %.2f%n", luas);
+    }
+}
